14e383574dfbe13a26415516c57cb9adcb36d49a
Dominique <domi.rau@gmail.com>
2017-09-09 18:50:25 +0200

    feat(status): add status

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const PORT = (process.env.PORT || 5000);
 app.use(bodyParser.json())
 
 server.listen(PORT);
+app.get('/', function(req, res) {
+	res.send('WORKING :)');
+})
 app.post('/publish', function (req, res) {
 	console.log('getting data', req.body);
 	io.emit('message', req.body);
